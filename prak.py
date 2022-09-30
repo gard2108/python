afc66913147fc5579f00773ca8a492a45ff6d331
ilia <ilyha.strykov@yandex.ru>
2022-09-30 16:33:09 +0300

    2 задачи

--- prak.py
+++ prak.py
@@ -7,3 +7,14 @@
 # #     print('a не квадрат b') 
 
 # Какое из 5 введенных чисел максимальное
+
+# spis = []
+# for i in range(5):
+#     spis.append(int((input('введите число'))))
+# print(spis)
+
+# max = spis[0]
+# for i in range(len(spis)):
+#     if max < spis[i]:
+#         max = spis[i]
+# print(max)
